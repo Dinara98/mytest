e889492588ae388818ae041a86c17e242075600e
Karabalaeva Dinara <karabalaeva.d98@mail.ru>
2017-10-02 11:05:51 +0600

    екінші коммит

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -3,6 +3,8 @@ package kz.kaznitu.lessons;
 public class Hello {
     public static void main (String [] args) {
         System.out.println("Hello World!");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты:");
+        System.out.println("Git Hub-пен жұмыс жасап үйрену");
     }
 
 }
