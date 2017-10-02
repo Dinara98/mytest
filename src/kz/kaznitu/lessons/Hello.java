ed0b060897b58e9f10e37e522aa3291dee7ba493
Karabalaeva Dinara <karabalaeva.d98@mail.ru>
2017-10-02 11:34:27 +0600

    Hello.java аздап ozgerdi

--- src/kz/kaznitu/lessons/Hello.java
+++ src/kz/kaznitu/lessons/Hello.java
@@ -5,6 +5,13 @@ public class Hello {
         System.out.println("Hello World!");
         System.out.println("Бұл зертханалық жұмыстың мақсаты:");
         System.out.println("Git Hub-пен жұмыс жасап үйрену");
+     int a = MyMath.kvadrat(5);
+     int b = MyMath.kub(5);
+
+        System.out.println("a = " + a) ;
+        System.out.println("b = " + b) ;
+        System.out.println(MyMath.max(8,9));
+
     }
 
 }
